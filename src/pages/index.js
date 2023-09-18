0631461064c6548ec3b5eb05e6bfc5e6c6c6357f
TheUncertaintim <yujeng.k@gmail.com>
2023-09-18 15:32:24 +0800

    Rename background image and adapt next/image component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,17 @@
-import Layout from "../components/layout";
+import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import bgImageNormal from "../../public/images/background-image-570px.png";
-import bgImageSmall from "../../public/images/background-image-285px.png";
-import a1Signage from "../../public/images/A1signage_web v3.png";
+import background from "../../public/images/a1-signage-web-v3.png";
 
 function LandingImage() {
   return (
     <div id={styles.container}>
-      <img
-        // srcSet={`${bgImageNormal.src} 570w, ${bgImageSmall.src} 285w`}
-        sizes="(max-width: 600px) 285px, 570px"
-        src={a1Signage.src}
-        alt="a logo with date of the wedding"
+      <Image
+        src={background}
+        width={1000}
+        height={600}
+        alt="A logo with date of the wedding"
         style={{ maxWidth: "100%", margin: "5vh auto" }}
       />
-      {/* <img src={a1Signage.src} alt="background" /> */}
     </div>
   );
 }
