23a9bf4c5b7cc422b0b4004a43d9991da3f866a0
TheUncertaintim <yujeng.k@gmail.com>
2023-09-30 12:37:49 +0800

    Handle error in MessageBoard

--- src/components/forms/message-board.js
+++ src/components/forms/message-board.js
@@ -6,23 +6,36 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function MessageBoard() {
   const { data, error, isLoading } = useSWR("/api/cards", fetcher);
-  if (error) {
-    // TODO: handle error!!!
-    console.log("error: ", error);
-  }
 
-  if (data && data.length == 0) {
-    return <p>Want to be the first one to give your advice?</p>;
-  } else {
+  if (error) {
+    // error fetching the messages
     return (
-      <>
+      <div role="alert">
+        <span>Oh oh...Messages failed to be loaded. Come back later?</span>;
+      </div>
+    );
+  } else if (isLoading) {
+    return (
+      <div>
+        <span>Loading other people&apos;s suggestions</span>
+      </div>
+    );
+  } else if (data && data.length == 0) {
+    // no message at all
+    return (
+      <div>
+        <span>Hey! No one left a message yet. Wanna be the first one?</span>;
+      </div>
+    );
+  } else {
+    // display messages
+    return (
+      <div>
         <h1>Read more from our friends and family:</h1>
-        {isLoading && <label>Loading other people&apos;s suggestions</label>}
-        {data &&
-          data.map((msg, idx) => (
-            <Message key={idx} category={msg.category} msg={msg} />
-          ))}
-      </>
+        {data.map((msg, idx) => (
+          <Message key={idx} category={msg.category} msg={msg} />
+        ))}
+      </div>
     );
   }
 }
