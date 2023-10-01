1ead91cfc356036437bb07d1c94f965423a7d531
TheUncertaintim <yujeng.k@gmail.com>
2023-10-01 10:50:22 +0800

    Add extra vertical space for footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
-    <>
+    <footer>
       <div>Yu-Jeng Kuo © 2023 </div>
       <div>
         <a href="https://github.com/TheUncertaintim/yjsh-day">GitHub</a>
       </div>
-    </>
+    </footer>
   );
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -9,27 +9,29 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>Interactive Marriage Advice Card</title>
+        <title>Yu-Jeng & ShaoHui&apos; Day</title>
         <meta
           name="description"
-          content="An interactive website for the wedding guests sending their wishes and memories to the newlyweds"
+          content="An digital wedding guestbook for Yu-Jeng & ShaoHui's wedding receptions"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <Header />
-      <main>
-        {isPageWide && (
-          <>
-            <div className={style.leftTitle}>YU-JENG & SHAOHUI</div>
-            <div className={style.rightTitle}>
-              CELEBRATING LOVE, LIFE, KIN/FRIENDSHIPS
-            </div>
-          </>
-        )}
-        <div className={style.pageLayout}>{children}</div>
-      </main>
-      <Footer />
+      <div className={style.bodyContainer}>
+        <Header />
+        <main>
+          {isPageWide && (
+            <>
+              <div className={style.leftTitle}>YU-JENG & SHAOHUI</div>
+              <div className={style.rightTitle}>
+                CELEBRATING LOVE, LIFE, KIN/FRIENDSHIPS
+              </div>
+            </>
+          )}
+          <div className={style.pageLayout}>{children}</div>
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
