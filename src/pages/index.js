5742c117714dd82784755116b14e1b5b7a3befa3
TheUncertaintim <yujeng.k@gmail.com>
2023-10-01 15:17:33 +0800

    Edited info message on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,15 +7,26 @@ export default function LandingImage() {
   // https://stackoverflow.com/questions/71429343/is-google-app-engine-standard-environment-compatible-with-next-image
   // For this reason we're using traditional <img> tag here
   return (
-    <img
-      srcSet={`${landingImgSmall.src} 400w, ${landingImgNormal.src} 800w`}
-      sizes="(max-width: 600px) 400px, 800px"
-      src={landingImgNormal.src}
-      alt="A logo with date of the wedding"
-      style={{
-        maxWidth: "100%",
-        margin: "0 auto",
-      }}
-    />
+    <>
+      <h1>Thanks for check out our wedding guestbook!</h1>
+      <p>
+        The website has been archived. You won&apos;t be able to submit any new
+        messages or photos.
+      </p>
+      <p>
+        Nevertheless, feel free to poke around, or read more about it&nbsp;
+        <a href="https://theuncertaintim.com/portfolio/yjsh-day/">here</a>.
+      </p>
+      <img
+        srcSet={`${landingImgSmall.src} 400w, ${landingImgNormal.src} 800w`}
+        sizes="(max-width: 600px) 400px, 800px"
+        src={landingImgNormal.src}
+        alt="A logo with date of the wedding"
+        style={{
+          maxWidth: "100%",
+          margin: "0 auto",
+        }}
+      />
+    </>
   );
 }
